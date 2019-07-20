8adec014a84dc72964cefdaf27158f33f134691f
laraakhil <24322269+LaraAkhil@users.noreply.github.com>
2019-07-21 00:48:46 +0530

    Updates

---
+++ precache-manifest.642927dbd33b776d665bbce34d940e2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e5aa55977529b8d7d9a7774cbb31f3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bd17982db0852a0ff5ed",
+    "url": "/monsters-rolodex/static/css/main.d8140f78.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "bd17982db0852a0ff5ed",
+    "url": "/monsters-rolodex/static/js/main.28fcf362.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
